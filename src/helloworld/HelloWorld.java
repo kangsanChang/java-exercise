bc4de065917a84131c713dba410dba76baa2dfd3
kangsan <rkdtks1005@gmail.com>
2018-03-06 15:42:50 +0900

    initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		String[] names;
+		System.out.println("Hello world world world!");
+		System.out.println("반가워용");
+		
+		for(int i=1; i<10 ; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.println(i + " X "+ j + " = " + i*j);
+			}
+		}
+	}
+}
