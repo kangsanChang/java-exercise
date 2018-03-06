153d8436de81da28fbeb3d4dd10c77630f140d97
kangsan <rkdtks1005@gmail.com>
2018-03-06 15:52:52 +0900

    change print message

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,9 +2,7 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		String[] names;
-		System.out.println("Hello world world world!");
-		System.out.println("반가워용");
+		System.out.println("안녕 세상아!");
 		
 		for(int i=1; i<10 ; i++) {
 			for(int j=1; j<10; j++) {
